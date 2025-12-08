c29362971b584abc611d73c7a659fc105bcc6f01
Peter Wang <peter258471@gmail.com>
2025-12-09 00:46:36 +0800

    Update web app version

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "447590516"
+    serviceWorkerVersion: "3821919009"
   }
 });
